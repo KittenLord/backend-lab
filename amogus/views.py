2bd35d2ca15c01ba2533993c1cf155027c770276
KittenLord <vanya.braslavets@gmail.com>
2025-10-13 22:09:34 +0300

    adjust endpoint

--- amogus/views.py
+++ amogus/views.py
@@ -1,5 +1,5 @@
 from amogus import app
 
-@app.route("/")
+@app.route("/healthcheck")
 def healthcheck():
-    return "<html><head></head><body><h1>Amogus</h1></body><html>"
+    return "<html><head></head><body><h1>Amogus is very healthy</h1></body><html>"
